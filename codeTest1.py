64924523dfd09bbf1b1b1e2e0511f9bdaeec32c9
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2022-11-26 00:08:01 +0530

    Added basic stuff

--- codeTest1.py
+++ codeTest1.py
@@ -1,5 +1,19 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-# AAAAAAAAAAAAAAAA
+class myLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(BoxLayout, self).__init__(**kwargs)
+        # self.layout = BoxLayout(anchor_x='center', anchor_y='center')
+        self.add_widget(btn = Button(text = "Name: "))
+        layout.add_widget(btn)
+
+class myApp(App): 
+    def build(self):
+        # return myLayout()
+        return myLayout()
+
+myApp().run()
