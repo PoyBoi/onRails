f376b452c383f17b761770a03b5788ee5cb1501a
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2022-11-26 03:16:19 +0530

    Major changes

--- codeTest1.py
+++ codeTest1.py
@@ -3,24 +3,25 @@ from kivy.uix.label import Label
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.lang.builder import Builder
 
-# class myLayout(AnchorLayout):
-#     def __init__(self, **kwargs):
-#         super(myLayout, self).__init__(**kwargs)
-#         self.layout = AnchorLayout(anchor_x='right', anchor_y='bottom')
-#         # btn = 
-#         self.layout.add_widget(Button(text='Hello World'))
-#         # self.add_widget(Button(text = "Name: "))
-       
-
-# class deployMain(App): 
-#     def build(self):
-#         return myLayout()
+Builder.load_string('''
+<myLayout>:
+    anchor_x:'right'
+    anchor_y:'top'
+    BoxLayout:
+        orientation: 'vertical'
+        Button:
+            text: 'aaaaa'
+        Button:
+            text: 'bbbb'
+''')
 
 class myLayout(AnchorLayout):
     pass
 
 class deployMain(App):
-    pass
+    def build(self):
+        return myLayout()
 
 deployMain().run()
