9da8a5a9ae3b2313a015a603e5b255af3061aed0
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2022-11-26 04:01:00 +0530

    Made buttons

--- codeTest1.py
+++ codeTest1.py
@@ -5,22 +5,52 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.lang.builder import Builder
 
+from kivy.core.window import Window
+
+Window.clearcolor = (1, 1, 1, 1)
+
 Builder.load_string('''
 <myLayout>:
-    anchor_x:'right'
+    anchor_x:'center'
     anchor_y:'top'
+    # GridLayout:
+    #   cols: 2
     BoxLayout:
         orientation: 'vertical'
+        padding: 20
+        spacing: 50
         Button:
             text: 'aaaaa'
-        Button:
+            color: [0, 0, 0, 1]
+            # background_color: app.color
+            on_press: 
+        RoundedButton:
             text: 'bbbb'
-''')
+            color: [0, 0, 0, 1]
+            pos_hint: {'center_x': 0.5}
+            size_hint: (1, .3)
+            # background_color: (48/255, 84/255, 150/255, 1)
+            # background_normal: ''
+            on_press: 
 
+<RoundedButton@Button>
+    background_color: (0, 0, 0, 0)
+    background_normal: ''
+    canvas.before:
+        Color:
+            rgba: (48/255, 84/255, 150/255, 1)
+        RoundedRectangle:
+            size: self.size
+            pos: self.pos
+            radius: [58]
+ ''')
+
+ 
 class myLayout(AnchorLayout):
     pass
 
 class deployMain(App):
+    color = [10, 7, 5, 90]
     def build(self):
         return myLayout()
 
